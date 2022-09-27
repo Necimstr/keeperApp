285ffefabf808469ee502860f59614a6db556b6a
necim <necimlebm1999@gmail.com>
2022-09-27 18:02:54 +0100

    first Kepper Commit

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+
+
+function Footer() {
+var D = new Date().getFullYear;
+  return (
+    <p>Copyrights {D}</p>
+  )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Header() {
+  return (
+    <div>keeper</div>
+  )
+}
+
+export default Header
